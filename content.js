dae0469c3f0d45b3ccceb761a18310e695777fdc
MikeTheFyke <mikefyke@hotmail.com>
2020-02-07 14:04:00 -0500

    Started Unword dropdown list

--- content.js
+++ content.js
@@ -3,3 +3,7 @@ let elements = document.getElementsByTagName('a');
 for (elt of elements) {
     elt.style['background-color'] = '#FF00FF';
 }
+
+function myFunction() {
+    document.getElementById("unword-List-Container").classList.toggle("show");
+  }
