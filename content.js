0a9959b0916a8ee4c9a605f47c499b0dc2d14bd2
MikeTheFyke <mikefyke@hotmail.com>
2020-02-07 16:11:49 -0500

    Started unword list generator

--- content.js
+++ content.js
@@ -1,9 +1,18 @@
 let elements = document.getElementsByTagName('a');
 
+var unwordArray = [];
+var newUnword = "";
+
+
 for (elt of elements) {
     elt.style['background-color'] = '#FF00FF';
 }
 
+function unwordGenerator(){
+    newUnword = document.getElementsByClassName("wordInput").value;
+    console.log("newUnword" + newUnword);
+}
+
 function myFunction() {
-    document.getElementById("unword-List-Container").classList.toggle("show");
+    document.getElementById("unword-List").classList.toggle("show");
   }
